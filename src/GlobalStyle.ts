539fe6688584c4a0046d916d8ed1aa714ce45984
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2025-05-29 01:36:43 +0200

    Refactor code structure for improved readability and maintainability

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -1,73 +1,110 @@
 import "modern-normalize";
-// import "@fontsource/jetbrains-mono/400.css";
-// import "@fontsource/jetbrains-mono/500.css";
-// import "@fontsource/geist-sans/400.css"; // Geist Regular
-// import "@fontsource/geist-sans/600.css"; // Geist SemiBold
-// import "@fontsource/geist-mono/600.css"; // Geist Mono SemiBold
 import { css } from "@emotion/react";
-// import GeistRegular from "../public/fonts/Geist/Geist-Regular.ttf";
-// import GeistSemiBold from "../public/fonts/Geist/Geist-SemiBold.ttf"
-// import GeistMono from "../public/fonts/Geist_mono/GeistMono-SemiBold.ttf"
-// import JetBrainsMonoMedium from "../public/fonts/JetBrains/JetBrainsMono-Medium.ttf"
-// import JetBrainsMonoRegular from "../public/fonts/JetBrains/JetBrainsMono-Regular.ttf"
+import FontAwesome5Free from "../src/assets/fonts/fa-regular-400.ttf"
+import Helvetica from "../src/assets/fonts/Helvetica.ttf"
+import HelveticaBold from "../src/assets/fonts/Helvetica-Bold.ttf"
+import HelveticaNeueRoman from "../src/assets/fonts/HelveticaNeueRoman.otf"
+import Inter_18ptMedium from "../src/assets/fonts/Inter_18pt-Medium.ttf"
+import Inter_18ptSemiBold from "../src/assets/fonts/Inter_18pt-SemiBold.ttf"
+import Inter_24ptRegular from "../src/assets/fonts/Inter_24pt-Regular.ttf"
+import PlusJakartaSans from "../src/assets/fonts/PlusJakartaSans[wght].ttf"
+import SFPRODISPLAYSEMIBOLDITALIC from "../src/assets/fonts/SFPRODISPLAYSEMIBOLDITALIC.OTF"
+
 export const GlobalStyle = css`
+  /* Font Awesome */
+  @font-face {
+    font-family: 'Font Awesome 5 Free';
+    src: url(${FontAwesome5Free}) format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  }
 
-  // @font-face {
-  //   font-family: 'Geist';
-  //   src: url()  format("truetype");
-  //   font-style: normal;
-  // }
+  /* Helvetica */
+  @font-face {
+    font-family: 'Helvetica';
+    src: url(${Helvetica}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
 
-  // @font-face {
-  //   font-family: 'Geist';
-  //   src: url() format("truetype");
-  //   font-style: normal;
-  // }
+  @font-face {
+    font-family: 'Helvetica';
+    src: url(${HelveticaBold}) format('truetype');
+    font-weight: bold;
+    font-style: normal;
+  }
 
-  // @font-face {
-  //   font-family: 'Geist Mono';
-  //   src: url() format("truetype");
-  //   font-style: normal;
-  // }
-  // @font-face {
-  //   font-family: 'JetBrainsMono Medium';
-  //   src: url() format("truetype");
-  //   font-style: normal;
-  // }
+  /* Helvetica Neue */
+  @font-face {
+    font-family: 'Helvetica Neue';
+    src: url(${HelveticaNeueRoman}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
 
-  // @font-face {
-  //   font-family: 'JetBrainsMono Regular';
-  //   src: url() format("truetype");
-  //   font-style: normal;
-  // }
+  /* Inter */
+  @font-face {
+    font-family: 'Inter';
+    src: url(${Inter_18ptMedium}) format('truetype');
+    font-weight: 500;
+    font-style: normal;
+  }
 
- 
+  @font-face {
+    font-family: 'Inter';
+    src: url(${Inter_18ptSemiBold}) format('truetype');
+    font-weight: 600;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Inter';
+    src: url(${Inter_24ptRegular}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  /* Plus Jakarta Sans */
+  @font-face {
+    font-family: 'Plus Jakarta Display';
+    src: url(${PlusJakartaSans}) format('truetype-variations');
+    font-weight: 100 900;
+    font-style: normal;
+  }
+
+  /* SF Pro */
+  @font-face {
+    font-family: 'SF Pro';
+    src: url(${SFPRODISPLAYSEMIBOLDITALIC}) format('opentype');
+    font-weight: 600;
+    font-style: italic;
+  }
+
   :root {
-	--black: #000;
-	--purple-800: #1a1f37;
-	--gray-500: #c4c4c4;
-	--purple-400: #8082ff;
-	--purple-500: #4c4eec;
-	--white-50: rgba(255, 255, 255, 0.5);
-	--white-75: rgba(255, 255, 255, 0.75);
-	--white-80: rgba(255, 255, 255, 0.8);
-	--white-60: rgba(255, 255, 255, 0.6);
-	--white-25: rgba(255, 255, 255, 0.25);
-	--white-70: rgba(255, 255, 255, 0.7);
-	--white-6: rgba(255, 255, 255, 0.06);
-	--white-24: rgba(255, 255, 255, 0.24);
-	--white-30: rgba(255, 255, 255, 0.3);
-	--white-100: #fff;
-  // !  fonts
+    --black: #000;
+    --purple-800: #1a1f37;
+    --gray-500: #c4c4c4;
+    --purple-400: #8082ff;
+    --purple-500: #4c4eec;
+    --white-50: rgba(255, 255, 255, 0.5);
+    --white-75: rgba(255, 255, 255, 0.75);
+    --white-80: rgba(255, 255, 255, 0.8);
+    --white-60: rgba(255, 255, 255, 0.6);
+    --white-25: rgba(255, 255, 255, 0.25);
+    --white-70: rgba(255, 255, 255, 0.7);
+    --white-6: rgba(255, 255, 255, 0.06);
+    --white-24: rgba(255, 255, 255, 0.24);
+    --white-30: rgba(255, 255, 255, 0.3);
+    --white-100: #fff;
+    
+    /* Fonts */
     --font-family: "Inter", sans-serif;
-  --second-family: "Plus Jakarta Display", sans-serif;
-  --third-family: "Helvetica", sans-serif;
-  --font3: "Font Awesome 5 Free", sans-serif;
-  --font4: "Helvetica Neue", sans-serif;
-  --font5: "SF Pro", sans-serif;
-}
-
-
+    --second-family: "Plus Jakarta Display", sans-serif;
+    --third-family: "Helvetica", sans-serif;
+    --font3: "Font Awesome 5 Free", sans-serif;
+    --font4: "Helvetica Neue", sans-serif;
+    --font5: "SF Pro", sans-serif;
+  }
 
   * {
     margin: 0;
@@ -89,15 +126,15 @@ export const GlobalStyle = css`
   }
     
   body.modal-open {
-  overflow: hidden;
-  position: fixed;
-  width: 100%;
-  height: 100%;
-}
+    overflow: hidden;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
 
   h1, h2, h3, h4, h5, h6 {
     font-weight: 600;
-    font-family: "Geist", sans-serif;
+    font-family: var(--second-family);
     color: #fff;
   }
 
@@ -107,12 +144,11 @@ export const GlobalStyle = css`
 
   a {
     text-decoration: none;
-     transition: all 0.3s ease-in-out;
+    transition: all 0.3s ease-in-out;
     &:hover {
       color: #808080;
-       transition: all 0.3s ease-in-out;
+      transition: all 0.3s ease-in-out;
     }
-  
   }
 
   img {
@@ -124,17 +160,14 @@ export const GlobalStyle = css`
 
   button {
     cursor: pointer;
-    // font-family: var(--second-family);
-    // background: linear-gradient(90deg, #e4e4e4 0%, #203a43 33.33%, #2c5364 66.66%, #e4e4e4 100%);
-    // background-size: 305% 100%;
-    // background-position: right bottom;
     transition: all 0.5s ease-in-out;
   }
-    video {
+  
+  video {
     object-fit: contain;
     overflow-clip-margin: content-box;
     overflow: clip;
-}
+  }
 
   ul, li {
     list-style: none;
