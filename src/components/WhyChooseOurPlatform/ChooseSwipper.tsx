d2bd1b60a2db62793564155c338226442a548354
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2025-12-27 20:31:12 +0100

    3 slide refactor

--- src/components/WhyChooseOurPlatform/ChooseSwipper.tsx
+++ src/components/WhyChooseOurPlatform/ChooseSwipper.tsx
@@ -7,7 +7,7 @@ import 'swiper/css';
 import point from '../../assets/icons/Traffic Lights (Big Sur).svg';
 import slide1 from '../../assets/icons/Sales Dashboard.png';
 import slide2 from '../../assets/icons/RestaurantDashboard.png';
-import slide3 from '../../assets/icons/Frame 3479-2.png';
+import slide3 from '../../assets/icons/ProductDashboard.png';
 import slide4 from '../../assets/icons/Frame 3479-3.png';
 import logo from '../../assets/icons/logo-srm.svg';
 import Tools from '../../assets/icons/Toolbar Group.svg';
